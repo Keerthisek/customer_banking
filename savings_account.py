1cfaece0111db0c4bda775b6f80161e9184c9796
Keerthisek <keerthisl@gmail.com>
2024-08-27 18:15:57 -0700

    Update savings_account.py

--- savings_account.py
+++ savings_account.py
@@ -2,12 +2,7 @@
 from Account import Account
 
 # Define a function for the Savings Account
-def create_savings_account(balance, interest_rate, months):
-    saving_account = Account(balance, interest_rate)
-    interest_earned = float(saving_account.balance)*float(interest_rate)/100*float(months)
-    saving_account.set_interest(interest_earned)
-    saving_account.set_balance(float(saving_account.balance)+float(interest_earned))
-    return saving_account.balance, saving_account.interest
+def create_savings_account(balance, interest_rate, months):    
     """Creates a savings account, calculates interest earned, and updates the account balance.
 
     Args:
@@ -19,21 +14,8 @@ def create_savings_account(balance, interest_rate, months):
         float: The updated savings account balance after adding the interest earned.
         And returns the interest earned.
     """
-    # Create an instance of the `Account` class and pass in the balance and interest parameters.
-    #  Hint: You need to add the interest as a value, i.e, 0.
-    
-
-    # Calculate interest earned
-    
-
-    # Update the savings account balance by adding the interest earned
-    
-
-    # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
-    
-
-    # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
-    
-
-    # Return the updated balance and interest earned.
-    
+    saving_account = Account(balance, interest_rate)
+    interest_earned = float(saving_account.balance)*float(interest_rate)/100*float(months)
+    saving_account.set_interest(interest_earned)
+    saving_account.set_balance(float(saving_account.balance)+float(interest_earned))
+    return saving_account.balance, saving_account.interest
